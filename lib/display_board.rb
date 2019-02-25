871854a1e5969ef118a6320832e97748132795fc
Ade Adesina <janet.adesina@gmail.com>
2019-02-25 00:21:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts board
-  puts rows = ["  |   |   ", "-----------",  "  |   |   ", "-----------", "   |   |   "]
+  puts rows = ["  |   |   ", "-----------",  "  |   |   ", "-----------", " |   |   "]
 end 
 
 display_board(board)
